521012951b13bc4146491481882f94786bf2d3e3
Lex <alext@disroot.org>
2022-07-03 00:15:39 +0200

    let, const, var

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,13 @@
+// LECTURE: Values and Variables
+
+let country = "The Netherlands";
+let continent = "Europe";
+let population = 17;
+console.log(country, continent, population)
+console.log(true)
+
+// LECTURE: Data Types
+
+let isIsland = false;
+let language;
+console.log(typeof isIsland,typeof population,typeof country,typeof language)
